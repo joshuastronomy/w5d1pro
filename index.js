cde80916ae017aa9c2f5bf05006babcd68a93c7d
Joshua Flack <joshu.alexander@gmail.com>
2017-06-19 17:41:15 -0400

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const lorem = require('lorem-ipsum');
 
 const lr = express();
-const output = lorem();
+const output = lorem({
+  count: 3                      // Number of words, sentences, or paragraphs to generate.
+, units: 'paragraphs'            // Generate words, sentences, or paragraphs.
+, sentenceLowerBound: 5         // Minimum words per sentence.
+, sentenceUpperBound: 15        // Maximum words per sentence.
+, paragraphLowerBound: 3        // Minimum sentences per paragraph.
+, paragraphUpperBound: 7        // Maximum sentences per paragraph.
+, format: 'html'
+});
 
 lr.get('/lorem', function (req, res) {
   res.send(output);
